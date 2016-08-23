2fd25888b61d2f4ca6ae51c57e357373eeb7689a
Konstantinos Samaras-Tsakiris <ksamtsak@gmail.com>
2016-08-23 11:52:59 +0300

    Parameterize mother wavelet

--- code/utils/eegcwt.m
+++ code/utils/eegcwt.m
@@ -1,4 +1,4 @@
-function [wcf, pfreq, scales] = eegcwt(eeg, fs, voicesPerOct)
+function [wcf, pfreq, scales] = eegcwt(eeg, fs, voicesPerOct, mwave)
 % eegcwt Calculate and show the cwt for each eeg channel
 % eeg: [channel]x[time] Matrix of eeg signals
 % wcf: [scales]x[time]x[channel] Matrix of wavelet coefficients
@@ -10,12 +10,12 @@ t= (0:size(eeg,2)-1)/fs;
 a0 = 1.67^(1/voicesPerOct);
 numoctaves = 10;
 scales = 4*a0.^(voicesPerOct:1/voicesPerOct:numoctaves*voicesPerOct);
-pfreq = scal2frq(scales,'morl',1/fs);
+pfreq = scal2frq(scales,mwave,1/fs);
 wcf= zeros(length(scales),length(t),channels);
 for i=1:channels
   % Calc wavelets
   x= eeg(i,:);
-  wcf(:,:,i) = cwt(x,scales,'morl');
+  wcf(:,:,i) = cwt(x,scales,mwave);
   % Plot
   wpfreqgram('image', wcf(:,:,i), pfreq, t,x);
 end
