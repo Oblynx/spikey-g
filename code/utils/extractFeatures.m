67444ef68ae11202d3e49c57d6078c403281d98e
Konstantinos Samaras-Tsakiris <ksamtsak@gmail.com>
2016-08-25 19:26:46 +0300

    Extract features from cwt

--- code/utils/extractFeatures.m
+++ code/utils/extractFeatures.m
@@ -12,23 +12,23 @@ for i= 1:size(w,3)
   w(:,:,i)= 100*x./sum(x(:));
 end
 
+f= zeros(6,size(eeg,1)); p= zeros(2,2,size(eeg,1)); pwidth= zeros(2,size(eeg,1));
 for i= 1:size(w,3)
   x= w(:,:,i);
-  x= imgaussfilt(x,wsmooth);
-  %{
-  p= FastPeakFind(x,max([min(max(x,[],1))  min(max(x,[],2))]), ...
-                  fspecial('gaussian', 10,2),5);
-  peaks= [p(1:2:end), p(2:2:end)];
-  figure; imagesc(x);
-  hold on; plot(peaks(:,1),peaks(:,2), 'rx');
-  %}
-  [zM,iM]= extrema2(x);
-  p= selectPeaks(x,iM, 0.2);
-  %figure; imagesc(flipud(x)); axis xy;
-  hold on; plot(t(p(:,1)), size(x,1)-p(:,2), 'rx');
+  x= imgaussfilt(x,wsmooth);  % Smoothen image
+  [~,iM]= extrema2(x);       % Find all the local maxima
+  p(:,:,i)= selectPeaks(x,iM, 0.2);
+  hold on; plot(t(p(:,1,i)), size(x,1)-p(:,2,i), 'mx'); % Highlight peaks
+  hold off;
+  pwidth(:,i)= peakWidth(x, p(:,:,i));
+  
+  f(:,i)= [w(p(1,2,i),p(1,1,i)), pfreq(p(1,2,i)), pwidth(1,i), ...
+           w(p(2,2,i),p(2,1,i)), pfreq(p(2,2,i)), pwidth(2,i)];
 end
 
 function p= selectPeaks(x, i, promThresh)
+% Select the 2 most prominent peaks (the 1st is always the total maximum)
+
 medx= median(x(:));
 [i(:,2), i(:,1)]= ind2sub(size(x), i);
 p(1,:)= i(1,:);
@@ -50,3 +50,17 @@ for ii=2:length(i)
   end
 end
 
+function pw= peakWidth(x,peaks)
+% Find the width of each peak
+pw= peaks(:,1);
+% For each peak find the area that is >10% peak height
+for p=1:size(peaks,1)
+  xp= x(peaks(p,2), peaks(p,1));
+  % Select the peak's column from the image
+  xu= x(peaks(p,2)+1:end, peaks(p,1));
+  xl= x(peaks(p,2)-1:-1:1, peaks(p,1));
+  % Cut to 10% peak height
+  xu= xu(1 : find(xu < 0.1*xp, 1)-1);
+  xl= xl(1 : find(xl < 0.1*xp, 1)-1);
+  pw(p)= std([xu;xl]);
+end
