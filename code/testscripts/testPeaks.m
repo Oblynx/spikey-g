73393de44037521ceed8ec406dd1e6ec79030927
Konstantinos Samaras-Tsakiris <ksamtsak@gmail.com>
2016-09-02 17:50:24 +0300

    Remove unnecessary test cases

--- code/testscripts/testPeaks.m
+++
@@ -1,7 +0,0 @@
-N=1000; gn=randn(N,1); gn= gn-mean(gn); a=cumsum(gn);
-a= conv(a,ones(5,1)/5,'same');
-figure(1);plot(a);
-
-[p,s]= kPeaks(10,a,1);
-
-hold on; scatter(p,a(p),30,'r'); hold off;
--- code/testscripts/testcwtftscales.m
+++ code/testscripts/testcwtftscales.m
@@ -1,15 +1,6 @@
 close all;
 t= (0:601)/1.6; a= [sin(2*pi*0.1*(0:300)/1.6), sin(2*pi*0.2*(0:300)/1.6)];
 figure; subplot(211); plot(t,a); subplot(212); plot(linspace(-0.8,0.8,602),fftshift(abs(fft(a))));
-
-
-
-
-
-
-
-
-
 %s0=1.25 ds=0.4875 nb=19
 scstr= struct('s0',1.25,'ds',0.2,'nb',32,'type','pow','pow',2);
 wstr= cwtft(a,'wavelet','morl', 'scales',scstr,'plot'); sc= wstr.scales; w= wstr.cfs;
